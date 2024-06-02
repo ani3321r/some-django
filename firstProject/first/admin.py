4ba34bd81daf14b319c2bac5c04517070176b392
ani3321r <ani3321r@gmail.com>
2024-06-02 14:11:56 +0530

    admin config

--- firstProject/first/admin.py
+++ firstProject/first/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from  .models import CarVariety
+from  .models import CarVariety, CarReview, Store, CarCertificate
 
 # Register your models here.
-admin.site.register(CarVariety)
+class CarReviewInline(admin.TabularInline):
+    model = CarReview
+    extra = 2
+
+class CarVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [CarReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('car_varities',)
+
+class CarCertificateAdmin(admin.ModelAdmin):
+    list_display = ('car', 'certificate_number')
+
+admin.site.register(CarVariety, CarVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(CarCertificate, CarCertificateAdmin)
