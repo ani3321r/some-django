b3ee216007998a8ec493ff2cca68bc97e141f795
ani3321r <ani3321r@gmail.com>
2024-06-03 15:38:30 +0530

    django forms

--- firstProject/first/urls.py
+++ firstProject/first/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.all_cars, name='all_cars'),
     path('<int:car_id>/', views.car_detail, name='car_detail'),
+    path('car_stores/', views.car_stores_view, name='car_stores'),
 ]
--- firstProject/first/views.py
+++ firstProject/first/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import CarVariety
+from .models import CarVariety, Store
 from django.shortcuts import get_object_or_404
+from .forms import CarVarietyForm
 
 # Create your views here.
 def all_cars(request):
@@ -10,3 +11,14 @@ def all_cars(request):
 def car_detail(request, car_id):
     car = get_object_or_404(CarVariety, pk=car_id)
     return render(request, 'first/car_detail.html', {'car':car})
+
+def car_stores_view(request):
+    stores = None
+    if request.method == 'POST':
+        form = CarVarietyForm(request.POST)
+        if form.is_valid():
+            car_variety = form.cleaned_data['car_variety']
+            Store.objects.filter(car_varities = car_variety)
+    else:
+        form = CarVarietyForm()
+    return render(request, 'first/car_stores.html',{'stores': stores, 'form': form})    
