a4ae7ed0b2134de0e6270a05a50df90d4ab5ec2c
ani3321r <ani3321r@gmail.com>
2024-07-07 23:42:42 +0530

    added templates

--- basicproject/xapp/urls.py
+++ basicproject/xapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.X_list, name='X_list'),
+    path("create/", views.X_create, name='X_create'),
+    path("<int:X_id>/edit/", views.X_edit, name='X_edit'),
+    path("<int:X_id>/delete/", views.X_delete, name='X_delete'),
 ]
